802d08bd86e72027746359b43b1d5f6bf07695f6
nojinee <dsg03-2@daun.met>
2023-10-13 15:33:26 +0900

    [FEAT] : 비밀번호 유효성검사 기능 적용

--- app/src/main/java/com/classic/museo/LoginActivity.kt
+++ app/src/main/java/com/classic/museo/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.classic.museo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.classic.museo.databinding.ActivityLonginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLonginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_longin)
+        binding = ActivityLonginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener{
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
